2426e05a4059ab2e68d5778ab48ed1c5b7b298af
Reylwane <107673099+Reylwane@users.noreply.github.com>
2022-06-24 05:23:44 -0700

    Add files via upload

---
+++ one.js
@@ -0,0 +1,30 @@
+// basic arithmetic calculator
+// ask user for the operator
+const operator = prompt('Enter operator ( +, -, * or / ): '),
+
+// ask user for the operands
+      firstNum = parseFloat(prompt('Enter first number: ')),
+      secondNum = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// calculation operations
+if (operator == '+') {
+    result = firstNum + secondNum;
+}
+else if (operator == '-') {
+    result = firstNum - secondNum;
+}
+else if (operator == '*') {
+    result = firstNum * secondNum;
+}
+else if (operator == '/') {
+    result = firstNum / secondNum;
+}
+else {
+    alert("Invalid operator")
+}
+
+// display the result
+alert(`${firstNum} ${operator} ${secondNum} = ${result}`);
+console.log(`${firstNum} ${operator} ${secondNum} = ${result}`);
